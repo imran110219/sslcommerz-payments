da2c2529310d4ac7601712f034cdd9c169372e6c
Sadman Sobhan <imran110219@gmail.com>
2024-12-10 19:03:45 +0600

    init

---
+++ src/main/java/io/github/imran110219/sslcommerzpayments/SslcommerzPaymentsApplication.java
@@ -0,0 +1,13 @@
+package io.github.imran110219.sslcommerzpayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslcommerzPaymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SslcommerzPaymentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/imran110219/sslcommerzpayments/SslcommerzPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.imran110219.sslcommerzpayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SslcommerzPaymentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
